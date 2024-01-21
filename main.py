aab2c92cc00bc7a916cdba1c7e11eb1d518f44b4
Vladtttj <151398469+Vladtttj@users.noreply.github.com>
2024-01-21 19:56:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+with open("quotes.txt", "r", encoding= "UTF-8") as file:
+    for line in file:
+        print(line)
+author = input("Хто написав ці рядки?")
+with open("quotes.txt", "a", encoding="UTF-8") as file:
+    file.write(f"({author}\n")
+
+while True:
+    answer = input("Бажаєте додати ще одну цитату? (Так/ Ні)")
+    answer = answer.lower()
+    if answer == "так":
+        quote = input("Введіть цитату:")
+        author = input("Введіть автора:")
+        file = open("quotes.txt", "a", encoding= "UTF-8")
+        file.write(f"{quote}\n({author})\n")
+        file.close()
+    else:
+        break
+with open("quotes.txt", "r", encoding= "UTF-8") as file:
+    for line in file:
+        print(line)
